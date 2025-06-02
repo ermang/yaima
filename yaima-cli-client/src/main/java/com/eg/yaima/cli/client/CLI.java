fd21f05998717b760be08812fa6579e46f0752e8
ermang <ermangonul17@gmail.com>
2025-06-02 11:56:13 +0300

    wip

--- yaima-cli-client/src/main/java/com/eg/yaima/cli/client/CLI.java
+++ yaima-cli-client/src/main/java/com/eg/yaima/cli/client/CLI.java
@@ -61,6 +61,13 @@ public class CLI {
                 dummy: bye
                 """, TextBox.Style.MULTI_LINE);
         chatTextBox.setReadOnly(true);
+        chatTextBox.setLayoutData(GridLayout.createLayoutData(
+                GridLayout.Alignment.BEGINNING, // Horizontal alignment in the grid cell if the cell is larger than the component's preferred size
+                GridLayout.Alignment.BEGINNING, // Vertical alignment in the grid cell if the cell is larger than the component's preferred size
+                true,       // Give the component extra horizontal space if available
+                false,        // Give the component extra vertical space if available
+                1,                  // Horizontal span
+                1));                  // Vertical
         contentPanel.addComponent(chatTextBox);
 
         friendPanel = new Panel(new GridLayout(1));
@@ -154,8 +161,9 @@ public class CLI {
                         if (key.getKeyType() == KeyType.Enter) {
                             System.out.println("enter pressed");
                             SendMessageCommand smc = new SendMessageCommand("dummy", friendButton.getLabel(), sendTextBox.getText());
-                            sendTextBox.setText("");
+
                             chatTextBox.setText(chatTextBox.getText() + "\nyou: " + sendTextBox.getText() + "\n");
+                            sendTextBox.setText("");
                             clientConnection.sendMessage(smc);
                             return false;
                         }
